a9474ff5ad3f410bdb7a15361d98b9da59d5a826
Ilya Aksenov <ais924@yandex.ru>
2024-07-09 14:01:55 +0300

    feature PM-06 добавлен конфиг претира (базово)

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,4 +1,4 @@
-import { logo } from "../../assets";
+import { logo } from "../../assets"
 import {
   calling,
   message,
@@ -7,22 +7,22 @@ import {
   twitter,
   instagram,
   linkedin,
-} from "../../assets/footer";
-import s from "./Footer.module.scss";
+} from "../../assets/footer"
+import s from "./Footer.module.scss"
 
 export function Footer() {
   const infoItem = [
     { name: "Профсоюзная улица, д.19", logo: location },
     { name: "ohuennyeZvety@pushka.ru", logo: message },
     { name: "8 999 831 28 22", logo: calling },
-  ];
+  ]
 
   const socialList = [
     { name: "facebook", logo: facebook },
     { name: "twitter", logo: twitter },
     { name: "instagram", logo: instagram },
     { name: "linkedin", logo: linkedin },
-  ];
+  ]
 
   return (
     <div className={s.root}>
@@ -31,7 +31,7 @@ export function Footer() {
         <div className={s.logoFooter}>Succulent store</div>
       </div>
       <div className={s.infoBlock}>
-        {infoItem.map((info) => (
+        {infoItem.map(info => (
           <div className={s.logoWrap}>
             <img src={info.logo} />
             <div className={s.logo}>{info.name}</div>
@@ -39,12 +39,12 @@ export function Footer() {
         ))}
       </div>
       <div className={s.social}>
-        {socialList.map((social) => (
+        {socialList.map(social => (
           <div className={s.socialLogoWrap}>
             <img src={social.logo} className={s.socialLogo} />
           </div>
         ))}
       </div>
     </div>
-  );
+  )
 }
